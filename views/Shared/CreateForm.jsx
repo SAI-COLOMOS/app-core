15452bf7374075dffcccdc64d86c5330c51abbf1
LordSinSentido <a19310166@ceti.mx>
2023-03-29 19:36:59 -0600

    Corrección error de vista del teclado

--- views/Shared/CreateForm.jsx
+++ views/Shared/CreateForm.jsx
@@ -1,6 +1,6 @@
 import { Flex, VStack, HStack } from "@react-native-material/core"
 import { ScrollView, KeyboardAvoidingView, Pressable } from "react-native"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { useSafeAreaInsets, useSafeAreaFrame } from "react-native-safe-area-context"
 import { Text, TouchableRipple, useTheme } from "react-native-paper"
 import { useEffect } from "react"
 
@@ -25,51 +25,46 @@ export default CreateForm = ({ navigation, route, loading, title, children, acti
   )
 
   return (
-    <Flex fill>
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "height" : "height"} style={{ width: "100%", height: "100%" }}>
-        <Flex fill style={{ backgroundColor: theme.colors.backdrop }} justify="end">
-          <Pressable
-            android_ripple={false}
-            style={{ width: "100%", height: "100%", position: "absolute" }}
-            onPress={() => {
-              if (!loading) {
-                navigation.pop()
-              }
-            }}
-          >
-            <Flex fill />
-          </Pressable>
+    <Flex fill justify="end" style={{ backgroundColor: theme.colors.backdrop }}>
+      <Pressable
+        android_ripple={false}
+        style={{ width: "100%", height: "100%", position: "absolute" }}
+        onPress={() => {
+          if (!loading) {
+            navigation.pop()
+          }
+        }}
+      />
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : null} style={{ maxHeight: "80%", justifyContent: "flex-end" }}>
+        <Flex
+          //fill
+          //maxH={"90%"}
+          // pb={insets.bottom}
+          style={{
+            backgroundColor: theme.colors.background,
+            borderTopLeftRadius: 50,
+            borderTopRightRadius: 50,
+            overflow: "hidden"
+          }}
+        >
+          <ScrollView>
+            <Flex p={25} items="center">
+              <Text variant="headlineMedium" style={{ textAlign: "center" }}>
+                {title}
+              </Text>
+            </Flex>
 
-          <Flex
-            maxH={"90%"}
-            pb={insets.bottom}
-            style={{
-              backgroundColor: theme.colors.background,
-              borderTopLeftRadius: 50,
-              borderTopRightRadius: 50,
-              overflow: "hidden"
-            }}
-          >
-            <ScrollView>
-              <Flex p={25} items="center">
-                <Text variant="headlineMedium" style={{ textAlign: "center" }}>
-                  {title}
-                </Text>
-              </Flex>
-
-              <VStack pr={25} pl={25} pb={50} spacing={30}>
-                {children.map((child, index) => (
-                  <Flex key={`Item ${index}`}>{child}</Flex>
-                ))}
-              </VStack>
-            </ScrollView>
-
-            <HStack justify="between" reverse={true} pv={20} ph={20}>
-              {actions.map((action, index) => (
-                <Flex key={`Action ${index}`}>{action}</Flex>
+            <VStack pr={25} pl={25} pb={50} spacing={30}>
+              {children.map((child, index) => (
+                <Flex key={`Item ${index}`}>{child}</Flex>
               ))}
-            </HStack>
-          </Flex>
+            </VStack>
+          </ScrollView>
+          <HStack justify="between" reverse={true} pv={20} ph={20}>
+            {actions.map((action, index) => (
+              <Flex key={`Action ${index}`}>{action}</Flex>
+            ))}
+          </HStack>
         </Flex>
       </KeyboardAvoidingView>
     </Flex>
