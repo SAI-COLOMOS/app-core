0f505a44e94f119c3e7c7456f1e6b7d4fd162845
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2023-04-05 22:37:45 -0600

    Implementación de la aplicación

--- views/Forms/FormDetails.jsx
+++ views/Forms/FormDetails.jsx
@@ -1,18 +1,20 @@
-import { Flex, VStack } from '@react-native-material/core'
-import { useCallback, useEffect, useState } from 'react'
-import { FlatList, RefreshControl, ScrollView } from 'react-native'
-import { ActivityIndicator, Avatar, Button, Card, FAB, ProgressBar, Text, useTheme } from 'react-native-paper'
-import { useHeaderHeight } from '@react-navigation/elements'
-import Constants from 'expo-constants'
-import Header from '../Shared/Header'
-import DisplayDetails from '../Shared/DisplayDetails'
-import { useFocusEffect } from '@react-navigation/native'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import { log } from 'react-native-reanimated'
+import { Flex, VStack } from "@react-native-material/core"
+import { useCallback, useContext, useEffect, useState } from "react"
+import { FlatList, RefreshControl, ScrollView } from "react-native"
+import { ActivityIndicator, Avatar, Button, Card, FAB, ProgressBar, Text, useTheme } from "react-native-paper"
+import { useHeaderHeight } from "@react-navigation/elements"
+import Constants from "expo-constants"
+import Header from "../Shared/Header"
+import DisplayDetails from "../Shared/DisplayDetails"
+import { useFocusEffect } from "@react-navigation/native"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+import { log } from "react-native-reanimated"
+import ApplicationContext from "../ApplicationContext"
 
 export default FormDetails = ({ navigation, route }) => {
   const localhost = Constants.expoConfig.extra.API_LOCAL
-  const { token, form_identifier } = route.params
+  const { token } = useContext(ApplicationContext)
+  const { form_identifier } = route.params
   const headerMargin = useHeaderHeight()
   const theme = useTheme()
 
@@ -24,15 +26,15 @@ export default FormDetails = ({ navigation, route }) => {
     setLoading(true)
 
     const request = await fetch(`${localhost}/forms/0101JEfyHr?isTemplate=${isTemplate}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
-        'Cache-Control': 'no-cache'
+        "Cache-Control": "no-cache"
       }
     })
-    .then((response) => (response.ok ? response.json() : response.status))
-    .catch((_) => null)
+      .then((response) => (response.ok ? response.json() : response.status))
+      .catch((_) => null)
 
     //const json = await request.json()
     //console.log("String: " + json)
@@ -51,7 +53,7 @@ export default FormDetails = ({ navigation, route }) => {
     navigation.setOptions({
       header: (props) => <Header {...props} />,
       headerTransparent: true,
-      headerTitle: 'Datos del formulario'
+      headerTitle: "Datos del formulario"
     })
   }, [])
 
@@ -63,15 +65,21 @@ export default FormDetails = ({ navigation, route }) => {
   )
 
   const Details = () => (
-    <Card key="Details" mode="outlined">
-      <VStack p={20} spacing={5}>
+    <Card
+      key="Details"
+      mode="outlined"
+    >
+      <VStack
+        p={20}
+        spacing={5}
+      >
         <Text variant="bodyLarge">Datos del formulario</Text>
         <VStack spacing={10}>
           <Flex>
             <Text variant="labelSmall">Descripción</Text>
             <Text variant="bodyMedium">{form?.description}</Text>
           </Flex>
-          
+
           <Flex>
             <Text variant="labelSmall">Registro del autor </Text>
             <Text variant="bodyMedium">{form?.author_register}</Text>
@@ -81,7 +89,7 @@ export default FormDetails = ({ navigation, route }) => {
             <Text variant="labelSmall">Área al que pertenece </Text>
             <Text variant="bodyMedium">{form?.belonging_area}</Text>
           </Flex>
-          
+
           <Flex>
             <Text variant="labelSmall">Parque al que pertenece </Text>
             <Text variant="bodyMedium">{form?.belonging_place}</Text>
@@ -119,18 +127,43 @@ export default FormDetails = ({ navigation, route }) => {
   )
 */
   return (
-    <Flex fill pt={headerMargin - 20}>
-      <ScrollView refreshControl={<RefreshControl refreshing={loading} onRefresh={(_) => getForm()} />}>
+    <Flex
+      fill
+      pt={headerMargin - 20}
+    >
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={loading}
+            onRefresh={(_) => getForm()}
+          />
+        }
+      >
         {form !== undefined ? (
           form !== null ? (
             isNaN(form) ? (
-              <DisplayDetails icon="form-select" title={form?.name} children={[Details()]} />
+              <DisplayDetails
+                icon="form-select"
+                title={form?.name}
+                children={[Details()]}
+              />
             ) : (
-              <VStack p={30} center spacing={20}>
-                <Icon color={theme.colors.onBackground} name="alert-circle-outline" size={50} />
+              <VStack
+                p={30}
+                center
+                spacing={20}
+              >
+                <Icon
+                  color={theme.colors.onBackground}
+                  name="alert-circle-outline"
+                  size={50}
+                />
                 <VStack center>
                   <Text variant="headlineSmall">Ocurrió un problema</Text>
-                  <Text variant="bodyMedium" style={{ textAlign: 'center' }}>
+                  <Text
+                    variant="bodyMedium"
+                    style={{ textAlign: "center" }}
+                  >
                     No podemos recuperar los datos del formulario, intentalo de nuevo más tarde (Error: {form})
                   </Text>
                 </VStack>
@@ -147,11 +180,22 @@ export default FormDetails = ({ navigation, route }) => {
               </VStack>
             )
           ) : (
-            <VStack center spacing={20} p={30}>
-              <Icon color={theme.colors.onBackground} name="wifi-alert" size={50} />
+            <VStack
+              center
+              spacing={20}
+              p={30}
+            >
+              <Icon
+                color={theme.colors.onBackground}
+                name="wifi-alert"
+                size={50}
+              />
               <VStack center>
                 <Text variant="headlineSmall">Sin internet</Text>
-                <Text variant="bodyMedium" style={{ textAlign: 'center' }}>
+                <Text
+                  variant="bodyMedium"
+                  style={{ textAlign: "center" }}
+                >
                   No podemos recuperar los datos del formulario, revisa tu conexión a internet e intentalo de nuevo
                 </Text>
               </VStack>
@@ -173,9 +217,9 @@ export default FormDetails = ({ navigation, route }) => {
       {!(form === undefined || form === null) ? (
         <FAB
           icon="pencil-outline"
-          style={{ position: 'absolute', margin: 16, right: 0, bottom: 0 }}
+          style={{ position: "absolute", margin: 16, right: 0, bottom: 0 }}
           onPress={() => {
-            navigation.navigate('AddSchool', {
+            navigation.navigate("AddSchool", {
               token,
               form
             })
